5e75a32adb3bdb289c3ff7984286a8a7bd982f0c
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-09-07 21:17:44 +0400

    Gamma finish

--- GitHub.Console/GitHub.Console/Program.cs
+++ GitHub.Console/GitHub.Console/Program.cs
@@ -11,9 +11,60 @@ namespace GitHub.Console
     {
         static void Main(string[] args)
         {
-            Ser_Deser_Json.Deserialize();
-            //Ser_Deser_Json.GetAllProfiles();
-            Ser_Deser_Json.GetAllRepo();
+            var profiles = GitInfo.Deserialize();
+            var allProfiles = GitInfo.GetAllProfiles();
+            var allRepos = GitInfo.GetAllRepo();
+            int countOfCSharpRepos = GitInfo.GetCSharpRepos();
+            var reposSortedByLanguage = GitInfo.SortByLanguages();
+            var bestReposByStars = GitInfo.SortByStars();
+            var repo2019 = GitInfo.Get2019Repo();
+
+            System.Console.WriteLine(new string('-', 30) + "\nURl Profiles.");
+
+            foreach (Item item in profiles)
+            {
+                System.Console.WriteLine(item.html_url);
+            }
+
+            System.Console.WriteLine(new string('-', 30) + "\nURL Repositorys.");
+
+            foreach (Repository item in allRepos)
+            {
+                System.Console.WriteLine(item.html_url);
+            }
+
+            System.Console.WriteLine(new string('-', 30) + "\nCount C# Repositorys: {0}", countOfCSharpRepos);
+            System.Console.WriteLine(new string('-', 30) + "\nRepositorys sorted by languages.");
+
+            foreach (var item in reposSortedByLanguage)
+            {
+                var q = item as Repository;
+                try
+                { 
+                    foreach (var item1 in (List<Repository>)item)
+                    {
+                        System.Console.WriteLine(new string(' ',2) + $"Profile name: {item1.owner.login} // Repository URL: {item1.html_url}");
+                    }
+                }
+                catch
+                {
+                    System.Console.WriteLine(item);
+                }
+            }
+
+            System.Console.WriteLine(new string('-', 30) + "\nBest Repositorys by stars.");
+
+            foreach (Repository item in bestReposByStars)
+            {
+                System.Console.WriteLine($"RepoURL: {item.html_url} {new string(' ', 20)} // Stars count: {item.stargazers_count}");
+            }
+
+            System.Console.WriteLine(new string('-', 30) + "\nURL Repositorys created at 2019.");
+
+            foreach (string item in repo2019)
+            {
+                System.Console.WriteLine(item);
+            }
         }
     }
 }
---
+++ GitHub.Console/GitHub.Lib/Gamma/GitInfo.cs
@@ -0,0 +1,125 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+
+namespace GitHub.Lib.Gamma//create_at.year
+{
+    public static class GitInfo
+    {
+        private static List<Item> profiles = new List<Item>();
+        private static List<URL_Rootobject> allPprofiles = new List<URL_Rootobject>();
+        private static List<Repository> allRepo = new List<Repository>();
+
+        public static IEnumerable Deserialize()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                string data = SendGetRequest("https://api.github.com/search/users?q=location:armenia&page={i}&per_page=100");
+                var persons = JsonConvert.DeserializeObject<Rootobject>(data);
+                profiles.AddRange(persons.items);
+            }
+            return profiles;
+        }
+
+        public static IEnumerable GetAllProfiles()
+        {
+            int i = 0;
+            foreach (var item in profiles)
+            {
+                string data = SendGetRequest(item.url);
+                URL_Rootobject persons = JsonConvert.DeserializeObject<URL_Rootobject>(data);
+                allPprofiles.Add(persons);
+
+                if (i++ == 15)
+                    break;
+            }
+            return allPprofiles;
+        }
+
+        public static IEnumerable GetAllRepo()
+        {
+            int i = 0;
+            foreach (var item in profiles)
+            {
+                string data = SendGetRequest(item.repos_url);
+                List<Repository> repos = JsonConvert.DeserializeObject<List<Repository>>(data);
+                allRepo.AddRange(repos);
+                if (i++ == 15)
+                    break;
+            }
+
+            return allRepo;
+        }
+
+        public static IEnumerable<Repository> SortByStars()
+        {
+            return allRepo.OrderBy(x => x.stargazers_count).Reverse();
+        }
+
+        public static int GetCSharpRepos()
+        {
+            return allRepo.Where(x => x.language == "C#").Select(x => x).ToList().Count;
+        }
+
+        private static List<string> GetLanguagesList()
+        {
+            return allRepo.GroupBy(x => x).Select(x => x.Key.language).Distinct().ToList();
+        }
+
+        public static IEnumerable SortByLanguages()
+        {
+            List<string> language = GetLanguagesList();
+            foreach (var lang in language)
+            {
+                List<Repository> repo1 = new List<Repository>();
+                foreach (var repository in allRepo)
+                {
+                    if (repository.language == lang)
+                    {
+                        repo1.Add(repository);
+                    }
+                }
+
+                yield return lang;
+                yield return repo1;
+            }
+        }
+
+        public static IEnumerable Get2019Repo()
+        {
+            return allRepo.Where(x => x.created_at.Year == 2019).Select(x => x.owner.html_url);
+        }
+
+
+
+        private static string SendGetRequest(string url)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Ssl3;
+
+            WebClient client = new WebClient();
+
+            string allGithubContentJson = null;
+            while (true)
+            {
+                try
+                {
+                    client.Headers.Add(HttpRequestHeader.UserAgent, "Anything");
+                    client.Headers.Add(HttpRequestHeader.ContentType, "applicaton/json");
+                    allGithubContentJson = client.DownloadString(url);
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return allGithubContentJson;
+        }
+    }
+
+}
